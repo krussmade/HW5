6d1b814eb06a5f177c5d0b4380b91770b26d87a5
_kruss <80170798+krussmade@users.noreply.github.com>
2021-12-15 23:59:19 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,27 +2,42 @@
 #include "headers/context.h"
 
 int main(int argc, char **argv) {
-//    if (argc < 2 || argc > 4) {
-//        std::cout << "Error!";
-//        exit(1);
-//    }
+    if (argc < 5 || argc > 6) {
+        std::cout << "Error!";
+        exit(1);
+    }
 
     Logger *logger;
+    int bee_count;
+    int volume_pot_honey;
+    int iter_count;
 
-//    if (!strcmp("-f", argv[0])) {
-//        logger = new FileLogger(argv[1]);
-//    } else if (!strcmp("-c", argv[0])) {
-//        if (argc != 4) {
-//            std::cout << "Error!";
-//            exit(1);
-//        }
+    if (!strcmp("-f", argv[1])) {
+        if (argc != 6) {
+            std::cout << "Error!";
+            exit(1);
+        }
+        logger = new FileLogger(argv[2]);
+        bee_count = atoi(argv[3]);
+        volume_pot_honey = atoi(argv[4]);
+        iter_count = atoi(argv[5]);
+    } else if (!strcmp("-c", argv[1])) {
+        if (argc != 5) {
+            std::cout << "Error!";
+            exit(1);
+        }
         logger = new ConsoleLogger;
-//    } else {
-//        std::cout << "Error!";
-//        exit(1);
-//    }
+        bee_count = atoi(argv[2]);
+        volume_pot_honey = atoi(argv[3]);
+        iter_count = atoi(argv[4]);
+    } else {
+        std::cout << "Error!";
+        exit(1);
+    }
 
-    Context context(logger, 2, 10, 10);
+    Context context(logger, bee_count, volume_pot_honey, iter_count);
     context.start();
+
+    delete logger;
     return 0;
 }
